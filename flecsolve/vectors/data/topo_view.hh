2fa7c17d0f0ba8c694e26785807a813f71bf4c08
Christopher Mauney <mauneyc@pn2305185.lanl.gov>
2024-03-07 08:40:56 -0700

    added accessor selection for different topos

--- flecsolve/vectors/data/topo_view.hh
+++ flecsolve/vectors/data/topo_view.hh
@@ -1,9 +1,41 @@
 #ifndef FLECSI_LINALG_VEC_DATA_TOPO_VIEW_HH
 #define FLECSI_LINALG_VEC_DATA_TOPO_VIEW_HH
 
+#include "flecsolve/topo/csr.hh"
 #include <flecsi/data.hh>
+#include <flecsi/topo/core.hh>
+#include <flecsi/topo/narray/interface.hh>
+#include <flecsi/topo/unstructured/interface.hh>
+#include <utility>
 
 namespace flecsolve::vec::data {
+
+/*
+ * select the topology accessor with the correct
+ * permissions
+ *
+ * we use `base` alias type defined in `flecsi::specialization`
+ * to check if the mesh shares the same core topo type.
+ *
+ * NOTE: flecsi itself doesn't have a great way to do this,
+ * so this is here until the that lib provides this.
+ */
+// the default case; this handles narray, csr.
+// these could be specialized explicitly but this
+// was less verbose
+template<class T, class = void>
+struct topo_acc_select {
+	using acc = typename T::template accessor<flecsi::ro>;
+};
+// the case of unstructured, with three permission bits
+template<class t>
+struct topo_acc_select<t,
+                       typename std::enable_if_t<std::is_same_v<
+						   typename t::base,
+						   flecsi::topo::base_t<flecsi::topo::unstructured>>>> {
+	using acc =
+		typename t::template accessor<flecsi::ro, flecsi::ro, flecsi::ro>;
+};
 
 template<typename T, flecsi::data::layout L = flecsi::data::layout::dense>
 using field = flecsi::field<T, L>;
@@ -29,7 +61,10 @@ struct topo_view {
 			flecsi::privilege_repeat<priv, num_priv - (num_priv > 1)>,
 			flecsi::privilege_repeat<flecsi::na, (num_priv > 1)>>;
 
-	using topo_acc = typename topo_t::template accessor<flecsi::ro>;
+	// select the topology accessor witt the correct number of
+	// permissions flags
+	using topo_acc = typename topo_acc_select<topo_t>::acc;
+
 	template<flecsi::partition_privilege_t priv>
 	using acc = typename field<scalar>::template accessor1<dofs_priv<priv>>;
 
